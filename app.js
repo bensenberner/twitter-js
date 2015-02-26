30a92316199ad393587fd2f88dc6e38443f67095
Ben Lerner <benlerner95@gmail.com>
2015-02-26 16:25:44 -0500

    finished the swig templating

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 var express = require( 'express' )
 var morgan = require( 'morgan' )
+var swig = require( 'swig' )
 var app = express()
 
 app.use(morgan('dev'))
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+swig.setDefaults({ cache: false });
+
+//app.get('/', function (req, res) {
+    //res.send('Hello, world!')
+//})
+
+var people = [{name: "Full"}, {name: 'Stacker'}, {name: "Son"}];
 
 app.get('/', function (req, res) {
-    res.send('Hello, world!')
-})
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
+});
 
 app.get('/news', function (req, res) {
     res.send('Hot off the presses: Yang wins Fields Medal!')
